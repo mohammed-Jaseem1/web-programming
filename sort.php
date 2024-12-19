68cb882ec576647b724bbddfabdca6b37b7bbe80
mohammed jaseem <147907994+mohammed-Jaseem1@users.noreply.github.com>
2024-12-19 12:58:25 +0530

    Add files via upload

---
+++ sort.php
@@ -0,0 +1,11 @@
+<?php
+$name = ['roshan','ayisha','nihal','linto','aami','jaseem'];
+echo "Displaying using print_r <br>";
+print_r($name);
+echo "<br> Sorting using asort <br>";
+asort($name);
+print_r($name);
+echo "<br> Sorting using asort <br>";
+arsort($name);
+print_r($name);
+?>
